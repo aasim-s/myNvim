e1cc13d40fd730af1fe182d136d40a0f01bd6c2c
aasim-s <aasimsayyed214@gmail.com>
2025-02-25 21:34:24 +0530

    migrated to primeagen version

---
+++ lua/theprimeagen/lazy/local.lua
@@ -0,0 +1,36 @@
+-- Return a table of plugin specs
+return {
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+          local mark = require 'harpoon.mark'
+          local ui = require 'harpoon.ui'
+
+          vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Add file to Harpoon' })
+          vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = 'Toggle Harpoon menu' })
+
+          vim.keymap.set('n', '<C-h>', function()
+            ui.nav_file(1)
+          end, { desc = 'Navigate to file 1' })
+          vim.keymap.set('n', '<C-t>', function()
+            ui.nav_file(2)
+          end, { desc = 'Navigate to file 2' })
+          vim.keymap.set('n', '<C-n>', function()
+            ui.nav_file(3)
+          end, { desc = 'Navigate to file 3' })
+          vim.keymap.set('n', '<C-s>', function()
+            ui.nav_file(4)
+          end, { desc = 'Navigate to file 4' })
+        end,
+    },
+    {
+        "ThePrimeagen/vim-apm",
+        config = function() end
+    },
+    {
+        "ThePrimeagen/vim-with-me",
+        config = function() end
+    }
+}
+
